3abccb24753f84ff68f6d0027992ef14ca2b70a6
Grant H <astrisms@gmail.com>
2016-06-07 16:31:57 -0700

    fixed messenger styling on components page

--- pages/index.js
+++ pages/index.js
@@ -86,12 +86,29 @@ export default class extends Component {
               <br notcode />
             </CodeCard>
             <CodeCard id={'messages'} title={'Messages'}>
-              <div className='msg-container' style={{maxHeight: '300px'}}>
-                <div className='msg msg-inbound'>Hey how are you?</div>
-                <div className='msg msg-outbound'>Great! Hows portland?</div>
-                <div className='msg msg-inbound'>I put a bird on it with my cold-pressed juice. Just got a ethical fingerstache tote bag, seitan meggings, selvage chartreuse and cray next-level biodiesel.</div>
-                <div className='msg msg-outbound'>I put a bird on it with my cold-pressed juice. Just got a ethical fingerstache tote bag, seitan meggings, selvage chartreuse and cray next-level biodiesel.</div>
-                <div className='msg msg-inbound'>I put a bird on it with my cold-pressed juice. Just got a ethical fingerstache tote bag, seitan meggings, selvage chartreuse and cray next-level biodiesel.</div>
+              <div className="msg-block card-block">
+                <div className='msg-container' style={{maxHeight: '300px'}}>
+                  <div className='msg-user msg-inbound'>Leo</div>
+                  <div className='msg msg-inbound'>
+                    Hey how are you?
+                  </div>
+                  <div className='msg-user msg-outbound'>You</div>
+                  <div className='msg msg-outbound'>
+                    Great! Hows portland?
+                  </div>
+                  <div className='msg-user msg-inbound'>Leo</div>
+                  <div className='msg msg-inbound'>
+                    I Put a bird on it with my cold-pressed juice. Just got a ethical fingerstache tote bag, seitan meggings, selvage chartreuse and cray next-level biodiesel.
+                  </div>
+                  <div className='msg-user msg-outbound'>You</div>
+                  <div className='msg msg-outbound'>
+                    I put a bird on it with my cold-pressed juice. Just got a ethical fingerstache tote bag, seitan meggings, selvage chartreuse and cray next-level biodiesel.
+                  </div>
+                  <div className='msg-user msg-outbound'>You</div>
+                  <div className='msg msg-outbound'>
+                    I put a bird on it with my cold-pressed juice. Just got a ethical fingerstache tote bag, seitan meggings, selvage chartreuse and cray next-level biodiesel.
+                  </div>
+                </div>
               </div>
             </CodeCard>
             <CodeCard id={'cards'} title={'Cards'}>
