54ccf385357979d5413b3422526c27efe9bbf489
Grant Helton <grant@gonimbly.com>
2016-06-29 15:22:07 -0700

    master fixed input-group-addon line height

--- pages/home.js
+++ pages/home.js
@@ -14,8 +14,8 @@ export default class extends Component {
     return (
       <div className='container'>
         <h1>Components</h1>
-        <div className="card">
-          <div className="card-block">
+        <div className='card'>
+          <div className='card-block'>
             <CodeCard id={'links'} title={'Links'}>
               <a href='#' className='btn btn-link btn-inline'>Link</a>
               <a href='#' className='btn btn-link btn-inline disabled'>Link disabled</a>
@@ -86,9 +86,9 @@ export default class extends Component {
               <br notcode />
             </CodeCard>
             <CodeCard id={'messages'} title={'Messages'}>
-              <div className="msg-block card-block">
+              <div className='msg-block card-block'>
                 <div className='msg-container' style={{minHeight: '300px'}}>
-                  <div className="msg-content">
+                  <div className='msg-content'>
                     <div className='msg-user msg-inbound'>Leo</div>
                     <div className='msg msg-inbound'>
                       Hey how are you?
@@ -114,11 +114,11 @@ export default class extends Component {
               </div>
             </CodeCard>
             <CodeCard id={'cards'} title={'Cards'}>
-              <div className="row">
-                <div className="col-sm-6">
+              <div className='row'>
+                <div className='col-sm-6'>
                   <div className='card text-xs-center'>
                     <div className='card-block'>
-                      <h4 className="card-title">card</h4>
+                      <h4 className='card-title'>card</h4>
                       <blockquote className='card-blockquote'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                         <footer>Someone famous in <cite title='Source Title'>Source Title</cite></footer>
@@ -127,7 +127,7 @@ export default class extends Component {
                   </div>
                   <div className='card card-inverse card-primary text-xs-center'>
                     <div className='card-block'>
-                      <h4 className="card-title">card card-inverse card-primary</h4>
+                      <h4 className='card-title'>card card-inverse card-primary</h4>
                       <blockquote className='card-blockquote'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                         <footer>Someone famous in <cite title='Source Title'>Source Title</cite></footer>
@@ -136,7 +136,7 @@ export default class extends Component {
                   </div>
                   <div className='card card-warning text-xs-center'>
                     <div className='card-block'>
-                      <h4 className="card-title">card card-warning</h4>
+                      <h4 className='card-title'>card card-warning</h4>
                       <blockquote className='card-blockquote'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                         <footer>Someone famous in <cite title='Source Title'>Source Title</cite></footer>
@@ -144,10 +144,10 @@ export default class extends Component {
                     </div>
                   </div>
                 </div>
-                <div className="col-sm-6">
+                <div className='col-sm-6'>
                   <div className='card card-inverse card-secondary text-xs-center'>
                     <div className='card-block'>
-                      <h4 className="card-title">card card-inverse card-secondary</h4>
+                      <h4 className='card-title'>card card-inverse card-secondary</h4>
                       <blockquote className='card-blockquote'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                         <footer>Someone famous in <cite title='Source Title'>Source Title</cite></footer>
@@ -156,7 +156,7 @@ export default class extends Component {
                   </div>
                   <div className='card card-inverse card-info text-xs-center'>
                     <div className='card-block'>
-                      <h4 className="card-title">card card-inverse card-info</h4>
+                      <h4 className='card-title'>card card-inverse card-info</h4>
                       <blockquote className='card-blockquote'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                         <footer>Someone famous in <cite title='Source Title'>Source Title</cite></footer>
@@ -165,7 +165,7 @@ export default class extends Component {
                   </div>
                   <div className='card card-inverse card-danger text-xs-center'>
                     <div className='card-block'>
-                      <h4 className="card-title">card card-inverse card-danger</h4>
+                      <h4 className='card-title'>card card-inverse card-danger</h4>
                       <blockquote className='card-blockquote'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                         <footer>Someone famous in <cite title='Source Title'>Source Title</cite></footer>
@@ -174,6 +174,30 @@ export default class extends Component {
                   </div>
                 </div>
               </div>
+            </CodeCard>
+            <CodeCard id={'inputs'} title={'inputs'}>
+              <form>
+                <fieldset className='form-group'>
+                  <label htmlFor='name'>Name of Business</label>
+                  <input type='text' className='form-control' id='name' />
+                </fieldset>
+                <fieldset className='form-group'>
+                  <label htmlFor='confirm'>Initial Confirmation</label>
+                  <div className='input-group'>
+                    <input type='number' className='form-control' />
+                    <span className='input-group-addon'>Days</span>
+                  </div>
+                  <small className='text-muted'>Number of days before appointment date.</small>
+                </fieldset>
+                <fieldset className='form-group'>
+                  <label htmlFor='confirm'>Reminder</label>
+                  <div className='input-group'>
+                    <input type='number' className='form-control' />
+                    <span className='input-group-addon'>Days</span>
+                  </div>
+                  <small className='text-muted'>Number of days after initial confirmation.</small>
+                </fieldset>
+              </form>
             </CodeCard>
           </div>
         </div>
