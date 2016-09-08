8d5680fabd9e90e0dc9474dd3d5a265e4aa6e404
Grant Helton <grant@gonimbly.com>
2016-09-08 15:25:06 -0700

    master updating static serving path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // routes
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get('/*', function(req, res) {
   res.render('index');
 });
