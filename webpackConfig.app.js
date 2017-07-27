4dd98c5ebae8ebaade0d4fdbcb56542aedda7b9f
Jon Crenshaw <jdcrensh@gmail.com>
2017-07-27 14:10:47 -0700

    Add build hooks for heroku and npm publish
    * Removes `dist/` since build hooks handle the builds

--- webpackConfig.app.js
+++ webpackConfig.app.js
@@ -1,80 +1,85 @@
 'use strict';
-var path = require('path');
-var webpack = require('webpack');
-var WebpackNotifierPlugin = require('webpack-notifier');
-var ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
-var plugins = [];
-var location;
-if(process.env.NODE_ENV === 'development') {
+const path = require('path');
+const webpack = require('webpack');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+const plugins = [];
+let location;
+if (process.env.NODE_ENV === 'development') {
   location = 'public';
   plugins.push(new WebpackNotifierPlugin());
   plugins.push(new ProgressBarPlugin());
 } else {
   location = 'dist';
-  plugins.push(new webpack.DefinePlugin({
-    'process.env':{
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }));
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-    sourceMap: true,
-    compress: {
-      warnings: true
-    }
-  }));
+  plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    })
+  );
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+      },
+    })
+  );
 }
 
-var config = {
+const config = {
   entry: './app.js',
   output: {
     path: path.join(__dirname, location),
-    filename: 'app.js'
+    filename: 'app.js',
   },
   stats: {
     colors: true,
     reasons: false,
-    assets: false
+    assets: false,
   },
+  devtool: 'sourcemap',
 
   resolve: {
-    extensions: ['.js', '.jsx', '.scss']
+    extensions: ['.js', '.jsx', '.scss'],
   },
   module: {
     loaders: [
-      {
-        // loads rules from .eslintrc.json
-        enforce: 'pre',
-        test: /\.jsx?$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
-      },
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          plugins: ['transform-object-rest-spread']
-        }
+          plugins: ['transform-object-rest-spread'],
+        },
       },
       {
         test: /\.scss/,
-        loader: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']
-      }, {
-         test: /\.(png|jpg|svg)$/,
-         loader: 'url-loader'
-      }
-    ]
+        loader: [
+          'style-loader',
+          'css-loader?sourceMap',
+          'postcss-loader',
+          'sass-loader?sourceMap',
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        loader: 'url-loader',
+      },
+    ],
   },
 
   plugins: plugins,
   externals: {
     // added for enzyme: https://github.com/airbnb/enzyme/blob/master/docs/guides/webpack.md
-    'cheerio': 'window',
+    cheerio: 'window',
     'react/addons': true,
     'react/lib/ExecutionEnvironment': true,
-    'react/lib/ReactContext': true
-  }
+    'react/lib/ReactContext': true,
+  },
 };
 
 module.exports = config;
--- webpackConfig.thumper.js
+++ webpackConfig.thumper.js
@@ -1,54 +1,69 @@
 'use strict';
-var path = require('path');
-var webpack = require('webpack');
-var WebpackNotifierPlugin = require('webpack-notifier');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var ProgressBarPlugin = require('progress-bar-webpack-plugin');
-var version = require('./package.json').version;
+
+const path = require('path');
+const webpack = require('webpack');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const version = require('./package.json').version;
 console.log('webpacking', version);
 
-var plugins = [
+const plugins = [
   new ProgressBarPlugin(),
   new WebpackNotifierPlugin(),
-  new webpack.BannerPlugin({banner: ['© Go Nimbly Inc.', new Date().getFullYear(), 'Thumper ' + version].join('\n'), entryOnly: true}),
   new ExtractTextPlugin({
-    filename:`[name].css`,
-    allChunks: true
-  })
+    filename: `[name].css`,
+    allChunks: true,
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true,
+    compress: {
+      warnings: false,
+    },
+  }),
+  new webpack.BannerPlugin({
+    banner: [
+      '© Go Nimbly Inc.',
+      new Date().getFullYear(),
+      `Thumper ${version}`,
+    ].join('\n'),
+    entryOnly: true,
+  }),
 ];
-var location;
-if(process.env.NODE_ENV === 'development') {
+let location;
+if (process.env.NODE_ENV === 'development') {
   location = 'public';
   plugins.push(new ExtractTextPlugin(`[name].css`));
 } else {
   location = 'dist';
 }
 
-var config = {
-  context: __dirname + '/src',
+const config = {
+  context: `${__dirname}/src`,
   entry: {
-    'thumper': './thumper.js', 
-    'thumper-scoped': './thumper-scoped.js'
+    thumper: './thumper.js',
+    'thumper-scoped': './thumper-scoped.js',
   },
+  devtool: 'sourcemap',
   output: {
     path: path.join(__dirname, location),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   stats: {
     colors: true,
     reasons: false,
-    assets: false
+    assets: false,
   },
 
   resolve: {
     extensions: ['.js', '.scss'],
     alias: {
-      'styles': __dirname + '/styles',
-      'mixins': __dirname + '/mixins',
-      'components': __dirname + '/components/',
-      'stores': __dirname + '/stores/',
-      'actions': __dirname + '/actions/'
-    }
+      styles: `${__dirname}/styles`,
+      mixins: `${__dirname}/mixins`,
+      components: `${__dirname}/components/`,
+      stores: `${__dirname}/stores/`,
+      actions: `${__dirname}/actions/`,
+    },
   },
   module: {
     // payment.js is required by card-react
@@ -58,23 +73,33 @@ var config = {
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           //resolve-url-loader may be chained before sass-loader if necessary
-          use: ['css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']
-        })
+          use: [
+            'css-loader?sourceMap',
+            'postcss-loader',
+            'sass-loader?sourceMap',
+          ],
+        }),
       },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: 'url-loader?limit=100000000&mimetype=application/font-woff' },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: 'url-loader?limit=100000000&mimetype=application/font-woff2' },
-      { test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,    loader: 'file-loader' }
-    ]
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=100000000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=100000000&mimetype=application/font-woff2',
+      },
+      { test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/, loader: 'file-loader' },
+    ],
   },
 
   plugins: plugins,
   externals: {
     // added for enzyme: https://github.com/airbnb/enzyme/blob/master/docs/guides/webpack.md
-    'cheerio': 'window',
+    cheerio: 'window',
     'react/addons': true,
     'react/lib/ExecutionEnvironment': true,
-    'react/lib/ReactContext': true
-  }
+    'react/lib/ReactContext': true,
+  },
 };
 
 module.exports = config;
